689fdced444a9549da535d2bce3c086473bf6900
Mohammad Honarmande <mohammadizi1401@gmail.com>
2026-05-17 09:14:08 -0700

    git

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 export default {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {}, 
+    extend: {
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+      },
+    },
   },
   plugins: [],
 };
