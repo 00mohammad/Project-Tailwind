48bfb2cf9144d7a7efae8e484a9bdfed56674866
Mohammad Honarmande <mohammadizi1401@gmail.com>
2026-05-19 10:05:14 -0700

    Everything is getting better.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,22 @@ export default {
           900: "#634832",
         },
       },
+
+      boxShadow: {
+        "shadow-normal": "0px 1px 10px rgba(0, 0, 0, 0.05)",
+      },
+
+      borderRadius: {
+        "4xl": "2rem",
+      },
+
+      fontFamily: {
+        dana: ["Dana", "sans-serif"],
+        danaMedium: ["Dana Medium", "sans-serif"],
+        danaDemiBold: ["Dana DemiBold", "sans-serif"],
+        morabbaLight: ["Morabba Light", "sans-serif"],
+        morabbaBold: ["Morabba Bold", "sans-serif"],
+      },
     },
   },
   plugins: [],
