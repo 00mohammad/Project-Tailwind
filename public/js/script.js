805f0e07a45afcf01662a5e0e73369002fe38ccc
Mohammad Honarmande <mohammadizi1401@gmail.com>
2026-06-07 12:30:32 -0700

    Navigation

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,18 @@
 const toggleThemeBtns = document.querySelectorAll(".toggle-theme");
 
 toggleThemeBtns.forEach((btn) => {
+
     btn.addEventListener("click", function () {
 
         if (localStorage.theme === "dark") {
             document.documentElement.classList.remove("dark");
             localStorage.theme = "light";
-        } else {
+        }
+         else {
             document.documentElement.classList.add("dark");
             localStorage.setItem("theme", "dark");
         }
 
     });
+
 });
